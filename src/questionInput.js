dab252aa1cda3ec276a18b791e56ddaedcab8bba
DanielDwyer <danielpatrickdwyer@gmail.com>
2018-03-26 21:27:39 -0600

    updates

--- src/questionInput.js
+++ src/questionInput.js
@@ -7,28 +7,37 @@ const questionPrompt = (dynamicGameData, InterfaceInstance) => {
     InterfaceInstance.question('\nWhat Category? (Please use the number to chose, not the name)\n$ ', (category) => {
 
       if((Number(category).toString()) == 'NaN' || Number(category) > 6){
-        questionPrompt(dynamicGameData, InterfaceInstance)
+        console.log("Please only answer using the number, 1, 2, 3...");
+        // resolve({boot: true})
+      }else{
+        InterfaceInstance.question('\nFor Which Value?\n$ ', (value) => {
+
+
+          if(value == 200){
+            var clueArrIndex = 0
+            resolve({category: category, clueArrIndex: clueArrIndex})
+          }else if(value == 400){
+            var clueArrIndex = 1
+            resolve({category: category, clueArrIndex: clueArrIndex})
+          }else if(value == 600){
+            var clueArrIndex = 2
+            resolve({category: category, clueArrIndex: clueArrIndex})
+          }else if(value == 800){
+            var clueArrIndex = 3
+            resolve({category: category, clueArrIndex: clueArrIndex})
+          }else if(value == 1000){
+            var clueArrIndex = 4
+            resolve({category: category, clueArrIndex: clueArrIndex})
+          }else{
+            console.log("Please only answer using the value, 200, 600, 800...");
+            // resolve({boot: true})
+          }
+
+
+
+        })
       }
 
-      InterfaceInstance.question('\nFor Which Value?\n$ ', (value) => {
-
-        if(value == 200){
-          var clueArrIndex = 0
-        }else if(value == 400){
-          var clueArrIndex = 1
-        }else if(value == 600){
-          var clueArrIndex = 2
-        }else if(value == 800){
-          var clueArrIndex = 3
-        }else if(value == 1000){
-          var clueArrIndex = 4
-        }else{
-          questionPrompt(dynamicGameData, InterfaceInstance)
-        }
-
-        resolve({category: category, clueArrIndex: clueArrIndex})
-
-      })
     })
   })
 
