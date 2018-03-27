fa76082344afa1654b69f21fced7b1ad989030b0
DanielDwyer <danielpatrickdwyer@gmail.com>
2018-03-27 10:49:44 -0600

    fixed the file system error, new logic for categories offset

--- src/categoriesStore.js
+++ src/categoriesStore.js
@@ -8,37 +8,71 @@ const fs = require('fs')
 const path = require('path')
 
 const categories = 6;
+var offsetBy = Math.floor(Math.random() * 18000);
 
 const categoriesStore = new Promise((resolve, reject) => {
 
-  fs.readFile(path.basename(__dirname) + '/categoryOffset.text', 'utf8', (err, offset) => {
-    if (err) {
-      reject(err);
-    } else {
-      fs.unlink(path.basename(__dirname) + '/categoryOffset.text', (err, success) => {
-        if (err) {
-          reject(err);
-        } else {
-          request('http://jservice.io/api/categories?count=' + categories + '&offset=' + offset, function(err, r, b) {
+  // fs.readFile(path.basename(__dirname) + '/categoryOffset.text', 'utf8', (err, offset) => {
+  //   if (err) {
+  //     reject(err);
+  //   } else {
+  //     fs.unlink(path.basename(__dirname) + '/categoryOffset.text', (err, success) => {
+  //       if (err) {
+  //         reject(err);
+  //       } else {
+          request('http://jservice.io/api/categories?count=' + categories + '&offset=' + offsetBy, function(err, r, b) {
             if (err) {
               reject(err);
             } else {
-              var replaceOffsetWith = Number(offset) + Number(categories)
-              fs.writeFile(path.basename(__dirname) + '/categoryOffset.text', replaceOffsetWith, (err) => {
-                if (err) {
-                  reject(err);
-                } else {
-                  resolve(b);
-                }
-              });
+              resolve(b);
+              // var replaceOffsetWith = Number(offset) + Number(categories)
+              // fs.writeFile(path.basename(__dirname) + '/categoryOffset.text', replaceOffsetWith, (err) => {
+              //   if (err) {
+              //     reject(err);
+              //   } else {
+              //     resolve(b);
+              //   }
+              // });
             }
           })
-        }
-      });
-    }
-
-  });
-
+//         }
+//       });
+//     }
+//
+//   });
+//
 });
+
+// const categoriesStore = new Promise((resolve, reject) => {
+//
+//   fs.readFile(path.basename(__dirname) + '/categoryOffset.text', 'utf8', (err, offset) => {
+//     if (err) {
+//       reject(err);
+//     } else {
+//       fs.unlink(path.basename(__dirname) + '/categoryOffset.text', (err, success) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           request('http://jservice.io/api/categories?count=' + categories + '&offset=' + offset, function(err, r, b) {
+//             if (err) {
+//               reject(err);
+//             } else {
+//               var replaceOffsetWith = Number(offset) + Number(categories)
+//               fs.writeFile(path.basename(__dirname) + '/categoryOffset.text', replaceOffsetWith, (err) => {
+//                 if (err) {
+//                   reject(err);
+//                 } else {
+//                   resolve(b);
+//                 }
+//               });
+//             }
+//           })
+//         }
+//       });
+//     }
+//
+//   });
+//
+// });
 
 exports.categoriesStore = categoriesStore
